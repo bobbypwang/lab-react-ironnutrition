ad74347dc5854208e42fdd207d9fe19ed160df60
Bobby Wang <bobby.p.wang@gmail.com>
2019-10-02 17:13:14 -0400

    Added the ability to add a new food item.

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,13 +4,28 @@ import './App.css';
 import completeFoodList from './foods.json';
 
 import FoodBox from './FoodBox';
+import Modal from './Modal';
 
-class App extends Component {                                                                                 
-
-  state = {
-    foods :completeFoodList
+class App extends Component {    
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      foods :completeFoodList,
+      modalState: false
+    };
+    
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
+  toggleModal() {    
+    this.setState((prev, props) => {
+      const newState = !prev.modalState;
+      
+      return { modalState: newState };
+    });
+  }
 
 
   render() {
@@ -19,11 +34,14 @@ class App extends Component {
         <section className="section">
         <div className="container">
           <h1 className="title is-1">Iron Nutrition</h1>
-          
-            {  console.log(this.state.foods)}
+            {console.log(this.state.foods)}
             <FoodBox importFoodsProp = {this.state.foods} />
         </div>
         </section>
+        <a id="add-food" className="button is-primary" onClick={this.toggleModal}>
+              Add New Food
+            </a>
+            <Modal closeModal={this.toggleModal} modalState={this.state.modalState} importFoodsProp={this.state.foods} footitle="Add New Food Item" />
       </div>
     );
   }
